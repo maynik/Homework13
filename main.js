5272d600caa2dc7fe8530f400b8b7332cc842532
maynik <bohdanmainov@gmail.com>
2024-12-13 12:51:32 +0200

    fix mistake

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ document.querySelector('.button-login').addEventListener('click', function(event
   event.preventDefault();
   document.getElementById('preloader').style.display = 'flex';
   setTimeout(function() {
-      window.location.href = '/registration.html';
+      window.location.href = 'https://maynik.github.io/Homework13/registration.html';
   }, 3000);
 });
